5ce66a5511c7a068bfd09b9fae70f4ff337f039f
Septian Nugraha <septian.adi.nugraha@gmail.com>
2017-12-15 16:34:40 +0700

    new html and methods for editing existing data.

--- src/main/java/id/go/bpkp/controller/ActorController.java
+++ src/main/java/id/go/bpkp/controller/ActorController.java
@@ -1,13 +1,18 @@
 package id.go.bpkp.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import id.go.bpkp.dao.ActorDAO;
+import id.go.bpkp.entity.Actor;
 
 @Controller
 @RequestMapping("/actor")
@@ -39,5 +44,27 @@ public class ActorController {
 		return "actor/costar";
 	}
 	
-
+	@GetMapping("/add")
+	public String add(Model model) {
+		model.addAttribute("actor", new Actor());
+		return "actor/add";
+	}
+	
+	@PostMapping("/add")
+	public String add(@Valid Actor actor, BindingResult result) {
+		actorDAO.addActor(actor);
+		return "redirect:/actor/index";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") int actorId, Model model) {
+		model.addAttribute("actor", actorDAO.getActor(actorId));
+		return "actor/edit";
+	}
+	
+	@PostMapping("/edit")
+	public String edit(Actor actor) {
+		actorDAO.editActor(actor);
+		return "redirect:/actor/index";
+	}
 }
--- src/main/java/id/go/bpkp/dao/ActorDAO.java
+++ src/main/java/id/go/bpkp/dao/ActorDAO.java
@@ -1,13 +1,17 @@
 package id.go.bpkp.dao;
 
+import java.util.Date;
 import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import id.go.bpkp.entity.Actor;
+import id.go.bpkp.entity.FilmActor;
 
 
 @Service
@@ -22,6 +26,50 @@ public class ActorDAO {
 	
 	public Actor getActor(int id) {
 		return (Actor) emf.createEntityManager().createQuery("from Actor where actorId = " + id).getSingleResult();
+	}
+	
+	public int addActor(Actor actor) {
+		
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction trans = em.getTransaction();
+		
+		int hasil = 0;
+		try {
+			trans.begin();
+			actor.setLastUpdate(new Date());
+			em.persist(actor);
+			trans.commit();
+		} catch (Exception e) {
+			trans.rollback();
+			hasil = -1;
+			System.out.println(e.getMessage());
+			// TODO: handle exception
+		}
+		return hasil;
+
+	}
+	
+	public int editActor(Actor actor) {
+		
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction trans = em.getTransaction();
+		
+		int hasil = 0;
+		try {
+			trans.begin();
+			Actor currentActor = em.find(Actor.class, actor.getActorId());
+			currentActor.setFirstName(actor.getFirstName());
+			currentActor.setLastName(actor.getLastName());
+			currentActor.setLastUpdate(new Date());
+			trans.commit();
+		} catch (Exception e) {
+			trans.rollback();
+			hasil = -1;
+			System.out.println(e.getMessage());
+			// TODO: handle exception
+		}
+		return hasil;
+
 	}
 
 
